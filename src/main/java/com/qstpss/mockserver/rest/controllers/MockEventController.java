4c9a5c67fda4aa45be837833bffe89f5b2326ca4
mikhail <m.tkachenko94@gmail.com>
2019-12-03 00:27:49 +0300

    implemented start and end phases

--- src/main/java/com/qstpss/mockserver/rest/controllers/MockEventController.java
+++ src/main/java/com/qstpss/mockserver/rest/controllers/MockEventController.java
@@ -2,11 +2,14 @@ package com.qstpss.mockserver.rest.controllers;
 
 import com.qstpss.mockserver.exceptions.EventAlreadyExistException;
 import com.qstpss.mockserver.exceptions.NotUniqueEventException;
+import com.qstpss.mockserver.model.Status;
 import com.qstpss.mockserver.model.entities.MockEvent;
 import com.qstpss.mockserver.services.MockEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/rest")
@@ -27,10 +30,22 @@ public class MockEventController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/edit")
-    private MockEvent editMockEvent(@RequestBody MockEvent mockEvent) {
+    @PutMapping("/start")
+    private MockEvent startMockEvent(@RequestBody MockEvent mockEvent) {
+        mockEvent.setStatus(Status.IN_PROGRESS);
+        mockEvent.setStartTimestamp(new Date());
         return mockEventService.update(mockEvent);
     }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/end")
+    private MockEvent endMockEvent(@RequestBody MockEvent mockEvent) {
+        mockEvent.setStatus(Status.CANCELED);
+        mockEvent.setEndTimestamp(new Date());
+        return mockEventService.update(mockEvent);
+    }
+
+    //todo get active all -> think about user experience
 
 
 }
